0e5bbf0f9e42ba6a4a52eafdbf99cdefc40a36b6
Vitor Nesello <vitornesello@gmail.com>
2018-11-10 21:23:25 +0100

    commit

--- test/run_examples.jl
+++ test/run_examples.jl
@@ -1,5 +1,5 @@
 using TimerOutputs
 
-@show "Running examples tests"
+println("Running examples tests")
 c = cron_test.CronStruct("exemple", 32, -123)
-@test cron_test.print_cron_struct(c) == "This is exemple. Has index 32 and a 
+@test cron_test.print_cron_struct(c) == "This is exemple. Has index 32 and a value of -123.0."
