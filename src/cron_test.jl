d16ebfd864ab30a5e8ea065c493714e9f0b65233
Vitor Nesello <vitornesello@gmail.com>
2018-11-10 17:31:11 +0100

    other commit

--- src/cron_test.jl
+++ src/cron_test.jl
@@ -1,5 +1,17 @@
 module cron_test
 
-greet() = print("Hello World!")
+greet() = print("Test cron!")
+
+struct CronStruct
+    id::String
+    index::Int
+    value::Float64
+end
+
+function print_cron_struct(object::CronStruct)
+    println("This is ", object.id, ". Has index ", object.index,
+            " and a value of ", object.value, ".")
+end
+
 
 end # module
