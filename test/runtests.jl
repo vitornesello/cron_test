a86be4b617b40380d3cad90ffb90db74510c0563
Vitor Nesello <vitornesello@gmail.com>
2018-11-10 20:50:15 +0100

    commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,9 @@ using Test
 using cron_test
 
 if length(ARGS) >= 1 && ARGS[1] == "cron"
-    #run different tests
     include("run_examples.jl")
-value of -123.0."
 else
+    println("running standard tests")
     c = cron_test.CronStruct("bid", 32, 0.673)
     @test cron_test.print_cron_struct(c) == "This is bid. Has index 32 and a value of 0.673."
 end
